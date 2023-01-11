ff03f9c986c0af17be4a697b9a4c0149915369bc
Pol de la Hoz <pol.delahoz@nicolis.com>
2023-01-11 14:04:37 +0100

    mig exercisi1

---
+++ T03N01/src/exercisi1/Month.java
@@ -0,0 +1,18 @@
+package exercisi1;
+
+public class Month {
+	
+	private String name;
+	
+	public Month(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
