3eea459666724f50fe1bac1a92a18e19151f8224
Pol de la Hoz <49729957+poldelahoz@users.noreply.github.com>
2023-01-12 17:26:26 +0100

    T04N01_exercisi1

---
+++ T04N01/src/exercisi1/MonthsList.java
@@ -0,0 +1,28 @@
+package exercisi1;
+
+import java.util.ArrayList;
+
+public class MonthsList {
+	
+	private final ArrayList<String> monthsList;
+	
+	public MonthsList() {
+		monthsList = new ArrayList<String>();
+		monthsList.add("Gener");
+		monthsList.add("Febrer");
+		monthsList.add("Març");
+		monthsList.add("Abril");
+		monthsList.add("maig");
+		monthsList.add("Juny");
+		monthsList.add("Juliol");
+		monthsList.add("Agost");
+		monthsList.add("Setembre");
+		monthsList.add("Octubre");
+		monthsList.add("Novembre");
+		monthsList.add("Desembre");
+	}
+	
+	public ArrayList<String> getMonthsList() {
+		return monthsList;
+	}
+}
