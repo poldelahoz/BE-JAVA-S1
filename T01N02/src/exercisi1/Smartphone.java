21e0a5641f30078aca189a98932246f63fdce444
Pol de la Hoz <49729957+poldelahoz@users.noreply.github.com>
2023-01-18 10:48:48 +0100

    T01N02 exercisi 1

---
+++ T01N02/src/exercisi1/Smartphone.java
@@ -0,0 +1,16 @@
+package exercisi1;
+
+public class Smartphone extends Telefon implements Camera, Rellotge{
+
+	public Smartphone(String marca, String model) {
+		super(marca, model);
+	}
+	
+	public void fotografiar() {
+		System.out.println("S’està fent una foto");
+	}
+	
+	public void alarma() {
+		System.out.println("Està sonant l’alarma");
+	}
+}
