fdfb7341d48a0b3c158ab3cf37b8c873c946fe63
Pol de la Hoz <49729957+poldelahoz@users.noreply.github.com>
2023-01-18 11:17:13 +0100

    annotation override

--- T01N02/src/exercisi1/Smartphone.java
+++ T01N02/src/exercisi1/Smartphone.java
@@ -6,10 +6,12 @@ public class Smartphone extends Telefon implements Camera, Rellotge{
 		super(marca, model);
 	}
 	
+	@Override
 	public void fotografiar() {
 		System.out.println("S’està fent una foto");
 	}
 	
+	@Override
 	public void alarma() {
 		System.out.println("Està sonant l’alarma");
 	}
