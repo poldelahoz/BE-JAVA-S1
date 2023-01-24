2d23f3de5dad318e94b59a87d04ad7c4f5011c57
Pol de la Hoz <49729957+poldelahoz@users.noreply.github.com>
2023-01-24 15:25:44 +0100

    T07N03 main

--- T07N03/src/exercisi1/Main.java
+++ T07N03/src/exercisi1/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			scanAnnotation();
+			scanAnnotations();
 			Alumne alumne = new Alumne("Harry", "Potter", 1, Alumne.Casa.Gryffindor);
 			ObjectToJsonConverter otjc = new ObjectToJsonConverter();
 			otjc.convertToJson(alumne);
@@ -16,24 +16,24 @@ public class Main {
 		}
 	}
 	
-	private static void scanAnnotation() {
+	private static void scanAnnotations() {
 		try {
 			Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass("exercisi1.Alumne");
 			JsonSerializable classAnnotation = clazz.getAnnotation(JsonSerializable.class);
 			if (classAnnotation != null)
 			{
-				System.out.println("La classe alumne té l'anotació personalitzada JsonSerializable amb valor savePath = " + classAnnotation.savePath());
+				System.out.println("La classe alumne té l'anotació personalitzada 'JsonSerializable' amb valor savePath = " + classAnnotation.savePath());
 			}else {
-				System.out.println("La classe alumne NO té l'anotació personalitzada JsonSerializable.");
+				System.out.println("La classe alumne NO té l'anotació personalitzada 'JsonSerializable'.");
 			}
 			System.out.println();
-			System.out.println("Atributs de la classe Alumne amb anotació personalitzada JsonElement: ");
+			System.out.println("Atributs de la classe Alumne amb anotació personalitzada 'JsonElement': ");
 			Field[] fields = clazz.getDeclaredFields();
 			for (Field field : fields) {
 			    if (field.isAnnotationPresent(JsonElement.class)) {
 			    	JsonElement fieldAnnotation = field.getAnnotation(JsonElement.class);
 			        System.out.println("Atribut: " + field.getName());
-			        System.out.println("Paràmetre nom de l'anotació JsonElement:" + fieldAnnotation.name());
+			        System.out.println("Paràmetre 'nom' de l'anotació 'JsonElement':" + fieldAnnotation.name());
 			    }
 			}
 			System.out.println();
